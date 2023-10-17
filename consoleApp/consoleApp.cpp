9ee30e02035c09a0c667725b6b2ab91e068e272c
Mike <amaglichc@gmail.com>
2023-10-17 15:00:20 +0300

    first version drawer

---
+++ consoleApp/consoleApp.cpp
@@ -0,0 +1,19 @@
+﻿using namespace std;
+#include <iostream>
+
+int main(){
+	int width, height;
+	char chr;
+	cout << "input a width:";
+	cin >> width;
+	cout << "input a height:";
+	cin >> height;
+	cout << "input a char:";
+	cin >> chr;
+	for (int i = 0; i<height; i++) {
+		for (int j=0; j<width; j++) {
+			cout << chr;  
+		}
+		cout<<""<<endl;
+	}
+} 
